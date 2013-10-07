598ef18ed24e01abc0718d2118f4848e27f3e39c
GarPit <garik.piton@gmail.com>
2013-10-07 23:39:21 +0400

    using correct table name setting

--- app/models/spree/prototype_property_relation.rb
+++ app/models/spree/prototype_property_relation.rb
@@ -1,6 +1,6 @@
 module Spree
   class PrototypePropertyRelation < ActiveRecord::Base
-    set_table_name 'spree_properties_prototypes'
+    self.table_name = 'spree_properties_prototypes'
     belongs_to :prototype
     belongs_to :property
   end
